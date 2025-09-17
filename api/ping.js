6520349c063d706ef9437b4858422899e42cc5de
Engr-Carlo <carlobisa2000@gmail.com>
2025-09-18 00:56:11 +0800

    adding check ping

---
+++ api/ping.js
@@ -0,0 +1,4 @@
+// Simple health-check to confirm /api functions are deployed
+export default function handler(req, res) {
+  res.status(200).json({ ok: true, route: '/api/ping', time: new Date().toISOString() });
+}
